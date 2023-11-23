7d96e4d2ac56d1d790fdff4eb91dc3144378d356
rwg070607 <133008024+rwg070607@users.noreply.github.com>
2023-11-23 20:31:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-const randomDiv = document.querySelector('div');
-const button = document.querySelector('button');
+const API_KEY = "022c44fda65f777ac64bc741ad25b005";
 
-function getRandomNum() {
-    const randomNum = Math.random();
-    randomDiv.innerText = randomNum;
+// https://openweathermap.org/current
+
+async function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  // const data = await fetch(url).then((response) => response.json());
+  const weather = document.querySelector("#weather div:first-child");
+  const city = document.querySelector("#weather div:last-child");
+  console.log(data);
+  // console.log(data.weather[0].main);
+  city.innerText = `${data.name}`;
+  weather.innerText = data.weather[0].main;
+}
+function onGeoError() {
+  alert();
 }
 
-button.addEventListener('click', getRandomNum);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
